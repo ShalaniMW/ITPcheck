62ffc4470acf18501d3bafa38e28ede265e76d60
ShalaniMW <shalawije@gmail.com>
2020-10-13 14:32:12 +0530

    init

--- springboot-backend/src/main/java/net/javaguides/springboot/repository/SupplierRepository.java
+++ springboot-backend/src/main/java/net/javaguides/springboot/repository/SupplierRepository.java
@@ -3,10 +3,9 @@ package net.javaguides.springboot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import net.javaguides.springboot.model.Employee;
+import net.javaguides.springboot.model.Supplier;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long>  {
-	
+public interface SupplierRepository extends JpaRepository<Supplier,Long> {
 
 }
